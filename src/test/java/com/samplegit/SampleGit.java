4c3daf98a8446be81044cfc8ccfa3fe505d09ffd
Indiradevi1 <indiradevik0523@gmail.com>
2025-09-17 13:05:08 +0530

    first push overall

---
+++ src/test/java/com/samplegit/SampleGit.java
@@ -0,0 +1,16 @@
+package com.samplegit;
+
+public class SampleGit {
+private void apple() {
+	System.out.println("one");
+
+}
+private void ball() {
+	System.out.println("one");
+
+}
+private void cat() {
+	System.out.println("one");
+
+}
+}
